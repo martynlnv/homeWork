d536ff9ad3ca23a588f03ae40e5faf402e24ed3d
martynln <martyn.lnv@gmail.com>
2025-03-04 20:00:25 +0300

    LES2

--- LES2/lesson_2_task_4.py
+++ LES2/lesson_2_task_4.py
@@ -5,7 +5,7 @@ def fizz_buzz(n):
 
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
-            print(f"{i} - Fizz, FizzBuzz")
+            print(f"{i} - FizzBuzz")
         elif i % 3 == 0:
             print(f"{i} - Fizz")
         elif i % 5 == 0:
